a598451e84830eeb3de4758d26a14109a47e8efd
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2025-07-29 23:17:28 +0300

    Finished Class Event.

--- Event.cpp
+++ Event.cpp
@@ -6,6 +6,9 @@
 
 #include <chrono>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <iomanip>
 
 unsigned int Event::getID() const {
     return id;
@@ -31,19 +34,19 @@ void Event::setDescription(const std::string &description) {
     descriere = description;
 }
 
-std::string Event::getDate() const {
+std::chrono::year_month_day Event::getDate() const {
     return data;
 }
 
-void Event::setDate(const std::string &date) {
+void Event::setDate(const std::chrono::year_month_day& date) {
     data = date;
 }
 
-std::string Event::getHour() const {
+std::chrono::hh_mm_ss<std::chrono::seconds> Event::getHour() const {
     return ora;
 }
 
-void Event::setHour(const std::string &hour) {
+void Event::setHour(const std::chrono::hh_mm_ss<std::chrono::seconds>& hour) {
     ora = hour;
 }
 
@@ -56,40 +59,74 @@ void Event::setLocation(const std::string &location) {
 }
 
 void Event::print() const {
-    std::cout<<"Detalii eveniment "<<titlu<<':'<<std::endl;
-    std::cout<<'\t'<<"Descriere eveniment: "<<descriere<<std::endl;
-    std::cout<<'\t'<<"Data eveniment "<<titlu<<':'<<data<<", ora: "<<ora<<std::endl;
-    std::cout<<'\t'<<"Locatie eveniment: "<<locatie<<std::endl;
+    std::cout << "Detalii eveniment " << titlu << ':' << std::endl;
+    std::cout << '\t' << "Descriere eveniment: " << descriere << std::endl;
+    // Folosim operatorul << supraîncărcat pentru tipurile chrono
+    std::cout << '\t' << "Data eveniment " << titlu << ':' << data << ", ora: " << ora << std::endl;
+    std::cout << '\t' << "Locatie eveniment: " << locatie << std::endl;
 }
 
 std::string Event::toString() const {
-    std::string str = "Detalii eveniment " + titlu + ':'+'\n'+ '\t'+ "Descriere eveniment: "+ descriere + '\n' +'\t' + "Data eveniment " + titlu + ':' + data + ", ora: " + ora + '\n' + '\t' + "Locatie eveniment: " + locatie + '\n';
-    return str;
+    std::stringstream ss;
+    ss << "Detalii eveniment " << titlu << ':' << '\n';
+    ss << '\t' << "Descriere eveniment: " << descriere << '\n';
+    ss << '\t' << "Data eveniment " << titlu << ':' << data << ", ora: " << ora << '\n';
+    ss << '\t' << "Locatie eveniment: " << locatie << '\n';
+    return ss.str();
 }
 
-bool Event::operator<(const Event &oth) const {
-    if (this->data == oth.data)
-        return this->ora < oth.ora;
-    return this->data < oth.data;
-}
+bool Event::operator<(const Event &other) const {
+    // Combinăm data și ora într-un time_point pentru o comparație corectă
+    auto this_time_point = std::chrono::sys_days(this->data) + this->ora.to_duration();
+    auto other_time_point = std::chrono::sys_days(other.data) + other.ora.to_duration();
 
+    return this_time_point < other_time_point;
+}
 std::string Event::serialize() const {
-    return std::to_string(id) + "|" + titlu + "|" + descriere + "|" + data + "|" + ora + "|" +  locatie;;
+    std::stringstream ss;
+    ss << id << "|" << titlu << "|" << descriere << "|" << data << "|" << ora << "|" << locatie;
+    return ss.str();
 }
 
 Event Event::deserialize(const std::string &line) {
-    std::stringstream ss(line);
+    std::stringstream line_ss(line);
     std::string part;
     std::vector<std::string> fields;
 
-    while (std::getline(ss, part, '|')) fields.push_back(part);
+    while (std::getline(line_ss, part, '|')) {
+        fields.push_back(part);
+    }
 
-    int id = std::stoi(fields[0]);
+    unsigned int id = std::stoul(fields[0]);
     std::string titlu = fields[1];
     std::string descriere = fields[2];
-    std::string data = fields[3];
-    std::string ora = fields[4];
     std::string locatie = fields[5];
 
-    return Event(id, titlu, descriere, data, ora, locatie);
+    std::stringstream data_ss(fields[3]);
+    std::string year_str, month_str, day_str;
+
+    std::getline(data_ss, year_str, '-');
+    std::getline(data_ss, month_str, '-');
+    std::getline(data_ss, day_str);
+
+    auto y = std::chrono::year{std::stoi(year_str)};
+    auto m = std::chrono::month{static_cast<unsigned int>(std::stoul(month_str))};
+    auto d = std::chrono::day{static_cast<unsigned int>(std::stoul(day_str))};
+    std::chrono::year_month_day parsed_data{y, m, d};
+
+    std::stringstream ora_ss(fields[4]);
+    std::string hour_str, min_str, sec_str;
+
+    std::getline(ora_ss, hour_str, ':');
+    std::getline(ora_ss, min_str, ':');
+    std::getline(ora_ss, sec_str);
+
+    auto h = std::chrono::hours{std::stol(hour_str)};
+    auto min = std::chrono::minutes{std::stol(min_str)};
+    auto s = std::chrono::seconds{std::stol(sec_str)};
+    std::chrono::hh_mm_ss<std::chrono::seconds> parsed_ora{h + min + s};
+
+    return Event(id, titlu, descriere, parsed_data, parsed_ora, locatie);
 }
+
+
--- Event.h
+++ Event.h
@@ -5,20 +5,29 @@
 #ifndef EVENT_H
 #define EVENT_H
 #include <string>
-
+#include <chrono>
 
 class Event {
 private:
     unsigned int id;
 
     std::string titlu;
+
     std::string descriere;
-    std::string data, ora;
+
+    std::chrono::year_month_day data;
+
+    std::chrono::hh_mm_ss<std::chrono::seconds> ora;
+
     std::string locatie;
 
 public:
     Event() = default;
-    Event(unsigned int ID, const std::string & title, const std::string & description, const std::string &date, const std::string & hour, const std::string & location): id(ID), titlu(title),descriere(description),data(date), ora(hour), locatie(location) {}
+    Event(unsigned int ID, const std::string & title, const std::string & description,
+          const std::chrono::year_month_day& date, const std::chrono::hh_mm_ss<std::chrono::seconds>& hour,
+          const std::string & location)
+        : id(ID), titlu(title), descriere(description), data(date), ora(hour), locatie(location) {}
+
 
     unsigned int getID() const;
     void setID(unsigned int ID);
@@ -29,11 +38,12 @@ public:
     std::string getDescription() const;
     void setDescription(const std::string & description);
 
-    std::string getDate() const;
-    void setDate(const std::string & date);
+    std::chrono::year_month_day getDate() const;
+    void setDate(const std::chrono::year_month_day& date);
 
-    std::string getHour() const;
-    void setHour(const std::string & hour);
+    std::chrono::hh_mm_ss<std::chrono::seconds> getHour() const;
+    void setHour(const std::chrono::hh_mm_ss<std::chrono::seconds>& hour);
+
 
     std::string getLocation() const;
     void setLocation(const std::string & location);
