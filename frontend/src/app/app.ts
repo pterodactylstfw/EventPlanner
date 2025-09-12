711e909944b89f1d254c56ba5808cac677cca9a5
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2025-09-12 17:46:30 +0300

    fix: event ID handling and enable CRUD via local storage for iOS

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
-import {Component, signal} from '@angular/core';
+import {Component, signal, ViewEncapsulation} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class App {
   protected readonly title = signal('frontend');
--- frontend/src/app/event.service.ts
+++ frontend/src/app/event.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {from, Observable} from 'rxjs';
 import {EventModel} from './event.model';
+import {Capacitor} from '@capacitor/core';
+import {EventLocalService} from './event-local.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +11,38 @@ import {EventModel} from './event.model';
 export class EventService {
   private apiUrl = 'http://localhost:18080';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private localService: EventLocalService) {
   }
 
   getEvents(): Observable<EventModel[]> {
+    if (this.isNativeIOS())
+      return from(this.localService.getEvents());
+
     return this.http.get<EventModel[]>(`${this.apiUrl}/events`)
   }
 
   addEvent(event: EventModel): Observable<any> {
+    if (this.isNativeIOS())
+      return from(this.localService.addEvent(event));
+
     return this.http.post(`${this.apiUrl}/add-event`, event, {responseType: 'text'});
   }
 
   updateEvent(event: EventModel): Observable<any> {
+    if (this.isNativeIOS())
+      return from(this.localService.updateEvent(event));
+
     return this.http.put(`${this.apiUrl}/events/${event.id}`, event, {responseType: 'text'});
   }
 
-  deleteEventService(id: number) {
+  deleteEventService(id: number): Observable<any> {
+    if (this.isNativeIOS()) {
+      return from(this.localService.deleteEvent(id));
+    }
     return this.http.delete(`${this.apiUrl}/events/${id}`, {responseType: 'text'})
+  }
+
+  private isNativeIOS(): boolean {
+    return Capacitor.isNativePlatform() && Capacitor.getPlatform() === 'ios';
   }
 }
